dbd2624bf00eb73becbd8ba8c5ad8a3773a993ed
Alexander Goryunov <43528668+agoryuno@users.noreply.github.com>
2022-10-10 15:39:21 +0300

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+inkscape cv_page1_plain.svg --export-pdf=goryunov_page1.pdf
+inkscape cv_page2_plain.svg --export-pdf=goryunov_page2.pdf
+
+pdftk goryunov_page1.pdf goryunov_page2.pdf cat output goryunov_cv.pdf
