113f7fab4c83cc0b19dfe2339f390558dd34295e
Alexander Goryunov <alex.goryunov@gmail.com>
2022-12-18 19:01:47 +0200

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 inkscape cv_page1_plain.svg --export-pdf=goryunov_page1.pdf
 inkscape cv_page2_plain.svg --export-pdf=goryunov_page2.pdf
 
-pdftk goryunov_page1.pdf goryunov_page2.pdf cat output goryunov_cv_tech.pdf
+pdftk goryunov_page1.pdf goryunov_page2.pdf cat output goryunov_cv.pdf
