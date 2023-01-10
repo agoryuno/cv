9b5a78c3724ce463642788dc06f21ad5ff543b75
Alexander Goryunov <alex.goryunov@gmail.com>
2023-01-10 16:34:25 +0200

    Fix paths

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ python make_qr.py
 python preprocess.py cv_page1_plain.svg --github=$GITHUB_URL
 python preprocess.py cv_page2_plain.svg --github=$GITHUB_URL
 
-inkscape $PWD/cv_page1_plain_out.svg --export-pdf=goryunov_page1.pdf
-inkscape $PWD/cv_page2_plain_out.svg --export-pdf=goryunov_page2.pdf
+inkscape cv_page1_plain_out.svg --export-pdf=goryunov_page1.pdf
+inkscape cv_page2_plain_out.svg --export-pdf=goryunov_page2.pdf
 
 pdftk goryunov_page1.pdf goryunov_page2.pdf cat output goryunov_cv.pdf
 
